4b0f2f5af3ab5ee24b4c3d9642ce487710266a1f
D3XK1M <dextermunene05@gmail.com>
2024-07-06 09:35:39 +0300

    updated header and slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className = "navbar header">
-            <div className = 'navbar-header header-centre'>
-                Link to "/" style = {{ textDecoration: 'none'}}><h3 className
-                = 'header-title'>Stock Market App</h3></Link>
+        <div className="navbar header">
+            <div className="navbar-header header-centre">
+                <Link to="/" style={{ textDecoration: 'none' }}>
+                    <h3 className="header-title">Stock Market App</h3>
+                </Link>
             </div>
         </div>
-    )
+    );
 }
 
 export default Header;
